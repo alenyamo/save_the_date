2ef5b66458c2c7579e206664bfd7d7fbb12cf6e8
aleks <alenyamo@gmail.com>
2022-05-18 15:53:22 -0400

    dev is done

---
+++ save_the_date/script.js
@@ -0,0 +1,21 @@
+
+  // $(".first").hide();
+  // $(".second").hide();
+  // $(".third").hide();
+
+  // setTimeout( function(){ 
+  //   $(".first").addClass(" animate__animated animate__jackInTheBox ");
+  //   $(".first").show();
+  // $(".second").addClass(" animate__animated animate__tada ");
+  // $(".second").show();
+  // $(".third").show();
+  // }  , 18000 );
+
+  // setTimeout( function(){ 
+  // $(".second").addClass(" animate__animated animate__tada ");
+  // $(".second").show();
+  // }  , 20000 );
+
+  // setTimeout( function(){ 
+  // $(".third").show();
+  // }  , 22000 );
